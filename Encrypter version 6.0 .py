72ab7839b17f624bf32d17fc91722997be390ecc
Emeraldemon <61049348+Emeraldemon@users.noreply.github.com>
2020-08-13 21:09:07 +0600

    Add files via upload

---
+++ Encrypter version 6.0 .py
@@ -0,0 +1,652 @@
+def encryptVI():
+    cin = str(input('enter individual alphabet of code and press enter: '))
+    if cin == "a":
+        cin = "4"
+    elif cin == "b":
+        cin = "$"
+    elif cin == "c":
+        cin = "="
+    elif cin == "d":
+        cin = "-"
+    elif cin == "e":
+        cin = "X"
+    elif cin == "f":
+        cin = "#"
+    elif cin == "g":
+        cin = "$"
+    elif cin == "o":
+        cin = "]]"
+    elif cin == "h":
+        cin = ";"
+    elif cin == "i":
+        cin = "+"
+    elif cin == "j":
+        cin = "P"
+    elif cin == "k":
+        cin = "IK"
+    elif cin == "l":
+        cin = '\+'
+    elif cin == "m":
+        cin = "\\"
+    elif cin == "n":
+        cin = "7"
+    elif cin == "p":
+        cin = "69"
+    elif cin == "q":
+        cin = "oU"
+    elif cin == "r":
+        cin = "1q"
+    elif cin == "s":
+        cin = "I="
+    elif cin == "t":
+        cin = "po"
+    elif cin == "u":
+        cin = "90"
+    elif cin == "v":
+        cin = "Xc"
+    elif cin == "w":
+        cin = "o#"
+    elif cin == "x":
+        cin = "pl"
+    elif cin == "x":
+        cin = "opl"
+    elif cin == "x":
+        cin = "@@"
+    elif cin == "y":
+        cin = "%4"
+    elif cin == "z":
+        cin = "^&"
+    cin2 = str(input('Do the same thing over here: '))
+    if cin2 == "a":
+        cin2 = "4"
+    elif cin2 == "b":
+        cin2 = "$"
+    elif cin2 == "c":
+        cin2 = "="
+    elif cin2 == "d":
+        cin2 = "-"
+    elif cin2 == "e":
+        cin2 = "X"
+    elif cin2 == "f":
+        cin2 = "#"
+    elif cin2 == "g":
+        cin2 = "$"
+    elif cin2 == "o":
+        cin2 = "]]"
+    elif cin2 == "h":
+        cin2 = ";"
+    elif cin2 == "i":
+        cin2 = "+"
+    elif cin2 == "j":
+        cin2 = "P"
+    elif cin2 == "k":
+        cin2 = "IK"
+    elif cin2 == "l":
+        cin2 = "\-0"
+    elif cin2 == "m":
+        cin2 = "\\"
+    elif cin2 == "n":
+        cin2 = "7"
+    elif cin2 == "p":
+        cin2 = "69"
+    elif cin2 == "q":
+        cin2 = "oU"
+    elif cin2 == "r":
+        cin2 = "1q"
+    elif cin2 == "s":
+        cin2 = "I="
+    elif cin2 == "t":
+        cin2 = "po"
+    elif cin2 == "u":
+        cin2 = "90"
+    elif cin2 == "v":
+        cin2 = "Xc"
+    elif cin2 == "w":
+        cin2 = "o#"
+    elif cin2 == "x":
+        cin2 = "pl"
+    elif cin2 == "x":
+        cin2 = "opl"
+    elif cin2 == "x":
+        cin2 = "@@"
+    elif cin2 == "y":
+        cin2 = "%4"
+    elif cin2 == "z":
+        cin2 = "^&"
+    cin22 = str(input('Repeat here: '))
+    if cin22 == "a":
+        cin22 = "4"
+    elif cin22 == "b":
+        cin22 = "$"
+    elif cin22 == "c":
+        cin22 = "="
+    elif cin22 == "d":
+        cin22 = "-"
+    elif cin22 == "e":
+        cin22 = "X"
+    elif cin22 == "f":
+        cin22 = "#"
+    elif cin22 == "g":
+        cin22 = "$"
+    elif cin22 == "o":
+        cin22 = "]]"
+    elif cin22 == "h":
+        cin22 = ";"
+    elif cin22 == "i":
+        cin22 = "+"
+    elif cin22 == "j":
+        cin22 = "P"
+    elif cin22 == "k":
+        cin22 = "IK"
+    elif cin22 == "l":
+        cin22 = "\}}"
+    elif cin22 == "m":
+        cin22 = "\\"
+    elif cin22 == "n":
+        cin22 = "7"
+    elif cin22 == "p":
+        cin22 = "69"
+    elif cin22 == "q":
+        cin22 = "oU"
+    elif cin22 == "r":
+        cin22 = "1q"
+    elif cin22 == "s":
+        cin22 = "I="
+    elif cin22 == "t":
+        cin22 = "po"
+    elif cin22 == "u":
+        cin22 = "90"
+    elif cin22 == "v":
+        cin22 = "Xc"
+    elif cin22 == "w":
+        cin22 = "o#"
+    elif cin22 == "x":
+        cin22 = "pl"
+    elif cin22 == "x":
+        cin22 = "opl"
+    elif cin22 == "x":
+        cin22 = "@@"
+    elif cin22 == "y":
+        cin22 = "%4"
+    elif cin22 == "z":
+        cin22 = "^&"
+    cin223 = str(input('Kindly repeat again: '))
+    if cin223 == "a":
+        cin223 =="4"
+    elif cin223 == "b":
+        cin223 = "$"
+    elif cin223 == "c":
+        cin223 =="="
+    elif cin223 == "d":
+        cin223 == "-"
+    elif cin223 == "e":
+        cin223 = "X"
+    elif cin223 == "f":
+        cin223 =="#"
+    elif cin223 == "g":
+        cin223 =="$"
+    elif cin223 == "o":
+        cin223 == "]]"
+    elif cin223 == "h":
+        cin223 == ";"
+    elif cin223 == "i":
+        cin223 == "+"
+    elif cin223 == "j":
+        cin223 =="P"
+    elif cin223 == "k":
+        cin223 == "IK"
+    elif cin223 == "l":
+        cin223 == "\}}"
+    elif cin223 == "m":
+        cin223 == "\\"
+    elif cin223 == "n":
+        cin223 == "7"
+    elif cin223 == "p":
+        cin223 = "69"
+    elif cin223 == "q":
+        cin223 = "oU"
+    elif cin223 == "r":
+        cin223 = "1q"
+    elif cin223 == "s":
+        cin223 = "I="
+    elif cin223 == "t":
+        cin223 = "po"
+    elif cin223 == "u":
+        cin223 = "90"
+    elif cin223 == "v":
+        cin223 = "Xc"
+    elif cin223 == "w":
+        cin223 = "o#"
+    elif cin223 == "x":
+        cin223 = "pl"
+    elif cin223 == "x":
+        cin223 = "opl"
+    elif cin223 == "x":
+        cin223 = "@@"
+    elif cin223 == "y":
+        cin223 = "%4"
+    elif cin223 == "z":
+        cin223 = "^&"
+    cin222 = str(input('Please bear with us and repeat again: '))
+    if cin222 == "a":
+        cin222 = "4"
+    elif cin222 == "b":
+        cin222 = "$"
+    elif cin222 == "c":
+        cin222 = "="
+    elif cin222 == "d":
+        cin222 = "-"
+    elif cin222 == "e":
+        cin222 = "X"
+    elif cin222 == "f":
+        cin222 = "#"
+    elif cin222 == "g":
+        cin222 = "$"
+    elif cin222 == "o":
+        cin222 = "]]"
+    elif cin222 == "h":
+        cin222 = ";"
+    elif cin222 == "i":
+        cin222 = "+"
+    elif cin222 == "j":
+        cin222 = "P"
+    elif cin222 == "k":
+        cin222 = "IK"
+    elif cin222 == "l":
+        cin222 = "\:"
+    elif cin222 == "m":
+        cin222 = "\\"
+    elif cin222 == "n":
+        cin222 = "7"
+    elif cin222 == "p":
+        cin222 = "69"
+    elif cin222 == "q":
+        cin222 = "oU"
+    elif cin222 == "r":
+        cin222 = "1q"
+    elif cin222 == "s":
+        cin222 = "I="
+    elif cin222 == "t":
+        cin222 = "po"
+    elif cin222 == "u":
+        cin222 = "90"
+    elif cin222 == "v":
+        cin222 = "Xc"
+    elif cin222 == "w":
+        cin222 = "o#"
+    elif cin222 == "x":
+        cin222 = "pl"
+    elif cin222 == "x":
+        cin222 = "opl"
+    elif cin222 == "x":
+        cin222 = "@@"
+    elif cin222 == "y":
+        cin222 = "%4"
+    elif cin222 == "z":
+        cin222 = "^&"
+    cin2222 = str(input('This is the last symbol before the result, type it and press enter: '))
+    if cin2222 == "a":
+        cin2222 = "4"
+    elif cin2222 == "b":
+        cin2222 = "$"
+    elif cin2222 == "c":
+        cin2222 = "="
+    elif cin2222 == "d":
+        cin2222 = "-"
+    elif cin2222 == "e":
+        cin2222 = "X"
+    elif cin2222 == "f":
+        cin2222 = "#"
+    elif cin2222 == "g":
+        cin2222 = "$"
+    elif cin2222 == "o":
+        cin2222 = "]]"
+    elif cin2222 == "h":
+        cin2222 = ";"
+    elif cin2222 == "i":
+        cin2222 = "+"
+    elif cin2222 == "j":
+        cin2222 = "P"
+    elif cin2222 == "k":
+        cin2222 = "IK"
+    elif cin2222 == "l":
+        cin2222 = "\p"
+    elif cin2222 == "m":
+        cin2222 = "\\"
+    elif cin2222 == "n":
+        cin2222 = "7"
+    elif cin2222 == "p":
+        cin2222 = "69"
+    elif cin2222 == "q":
+        cin2222 = "oU"
+    elif cin2222 == "r":
+        cin2222 = "1q"
+    elif cin2222 == "s":
+        cin2222 = "I="
+    elif cin2222 == "t":
+        cin2222 = "po"
+    elif cin2222 == "u":
+        cin2222 = "90"
+    elif cin2222 == "v":
+        cin2222 = "Xc"
+    elif cin2222 == "w":
+        cin2222 = "o#"
+    elif cin2222 == "x":
+        cin2222 = "pl"
+    elif cin2222 == "x":
+        cin2222 = "opl"
+    elif cin2222 == "x":
+        cin2222 = "@@"
+    elif cin2222 == "y":
+        cin2222 = "%4"
+    elif cin2222 == "z":
+        cin2222 = "^&"
+    #Exagerration
+    print("process complete")
+    resultVI = cin + cin2 + cin22 + cin222 + cin2222 + cin223
+    print("here's your code:",resultVI)
+    
+def encryptV():
+    cin = str(input('enter an individual symbol of code and press enter'))
+    if cin == "a":
+        cin = "4"
+    elif cin == "b":
+        cin = "$"
+    elif cin == "c":
+        cin = "="
+    elif cin == "d":
+        cin = "-"
+    elif cin == "e":
+        cin = "X"
+    elif cin == "f":
+        cin = "#"
+    elif cin == "g":
+        cin = "$"
+    elif cin == "o":
+        cin = "]]"
+    elif cin == "h":
+        cin = ";"
+    elif cin == "i":
+        cin = "+"
+    elif cin == "j":
+        cin = "P"
+    elif cin == "k":
+        cin = "IK"
+    elif cin == "l":
+        cin = '\+'
+    elif cin == "m":
+        cin = "\\"
+    elif cin == "n":
+        cin = "7"
+    elif cin == "p":
+        cin = "69"
+    elif cin == "q":
+        cin = "oU"
+    elif cin == "r":
+        cin = "1q"
+    elif cin == "s":
+        cin = "I="
+    elif cin == "t":
+        cin = "po"
+    elif cin == "u":
+        cin = "90"
+    elif cin == "v":
+        cin = "Xc"
+    elif cin == "w":
+        cin = "o#"
+    elif cin == "x":
+        cin = "pl"
+    elif cin == "x":
+        cin = "opl"
+    elif cin == "x":
+        cin = "@@"
+    elif cin == "y":
+        cin = "%4"
+    elif cin == "z":
+        cin = "^&"
+    cin2 = str(input('enter 2ND individual symbol of code and press enter: '))
+    if cin2 == "a":
+        cin2 = "4"
+    elif cin2 == "b":
+        cin2 = "$"
+    elif cin2 == "c":
+        cin2 = "="
+    elif cin2 == "d":
+        cin2 = "-"
+    elif cin2 == "e":
+        cin2 = "X"
+    elif cin2 == "f":
+        cin2 = "#"
+    elif cin2 == "g":
+        cin2 = "$"
+    elif cin2 == "o":
+        cin2 = "]]"
+    elif cin2 == "h":
+        cin2 = ";"
+    elif cin2 == "i":
+        cin2 = "+"
+    elif cin2 == "j":
+        cin2 = "P"
+    elif cin2 == "k":
+        cin2 = "IK"
+    elif cin2 == "l":
+        cin2 = "\-0"
+    elif cin2 == "m":
+        cin2 = "\\"
+    elif cin2 == "n":
+        cin2 = "7"
+    elif cin2 == "p":
+        cin2 = "69"
+    elif cin2 == "q":
+        cin2 = "oU"
+    elif cin2 == "r":
+        cin2 = "1q"
+    elif cin2 == "s":
+        cin2 = "I="
+    elif cin2 == "t":
+        cin2 = "po"
+    elif cin2 == "u":
+        cin2 = "90"
+    elif cin2 == "v":
+        cin2 = "Xc"
+    elif cin2 == "w":
+        cin2 = "o#"
+    elif cin2 == "x":
+        cin2 = "pl"
+    elif cin2 == "x":
+        cin2 = "opl"
+    elif cin2 == "x":
+        cin2 = "@@"
+    elif cin2 == "y":
+        cin2 = "%4"
+    elif cin2 == "z":
+        cin2 = "^&"
+    cin22 = str(input('enter 3RD individual symbol of code and press enter: '))
+    if cin22 == "a":
+        cin22 = "4"
+    elif cin22 == "b":
+        cin22 = "$"
+    elif cin22 == "c":
+        cin22 = "="
+    elif cin22 == "d":
+        cin22 = "-"
+    elif cin22 == "e":
+        cin22 = "X"
+    elif cin22 == "f":
+        cin22 = "#"
+    elif cin22 == "g":
+        cin22 = "$"
+    elif cin22 == "o":
+        cin22 = "]]"
+    elif cin22 == "h":
+        cin22 = ";"
+    elif cin22 == "i":
+        cin22 = "+"
+    elif cin22 == "j":
+        cin22 = "P"
+    elif cin22 == "k":
+        cin22 = "IK"
+    elif cin22 == "l":
+        cin22 = "\}}"
+    elif cin22 == "m":
+        cin22 = "\\"
+    elif cin22 == "n":
+        cin22 = "7"
+    elif cin22 == "p":
+        cin22 = "69"
+    elif cin22 == "q":
+        cin22 = "oU"
+    elif cin22 == "r":
+        cin22 = "1q"
+    elif cin22 == "s":
+        cin22 = "I="
+    elif cin22 == "t":
+        cin22 = "po"
+    elif cin22 == "u":
+        cin22 = "90"
+    elif cin22 == "v":
+        cin22 = "Xc"
+    elif cin22 == "w":
+        cin22 = "o#"
+    elif cin22 == "x":
+        cin22 = "pl"
+    elif cin22 == "x":
+        cin22 = "opl"
+    elif cin22 == "x":
+        cin22 = "@@"
+    elif cin22 == "y":
+        cin22 = "%4"
+    elif cin22 == "z":
+        cin22 = "^&"
+    cin222 = str(input('Repeat here: '))
+    if cin222 == "a":
+        cin222 = "4"
+    elif cin222 == "b":
+        cin222 = "$"
+    elif cin222 == "c":
+        cin222 = "="
+    elif cin222 == "d":
+        cin222 = "-"
+    elif cin222 == "e":
+        cin222 = "X"
+    elif cin222 == "f":
+        cin222 = "#"
+    elif cin222 == "g":
+        cin222 = "$"
+    elif cin222 == "o":
+        cin222 = "]]"
+    elif cin222 == "h":
+        cin222 = ";"
+    elif cin222 == "i":
+        cin222 = "+"
+    elif cin222 == "j":
+        cin222 = "P"
+    elif cin222 == "k":
+        cin222 = "IK"
+    elif cin222 == "l":
+        cin222 = "\:"
+    elif cin222 == "m":
+        cin222 = "\\"
+    elif cin222 == "n":
+        cin222 = "7"
+    elif cin222 == "p":
+        cin222 = "69"
+    elif cin222 == "q":
+        cin222 = "oU"
+    elif cin222 == "r":
+        cin222 = "1q"
+    elif cin222 == "s":
+        cin222 = "I="
+    elif cin222 == "t":
+        cin222 = "po"
+    elif cin222 == "u":
+        cin222 = "90"
+    elif cin222 == "v":
+        cin222 = "Xc"
+    elif cin222 == "w":
+        cin222 = "o#"
+    elif cin222 == "x":
+        cin222 = "pl"
+    elif cin222 == "x":
+        cin222 = "opl"
+    elif cin222 == "x":
+        cin222 = "@@"
+    elif cin222 == "y":
+        cin222 = "%4"
+    elif cin222 == "z":
+        cin222 = "^&"
+    cin2222 = str(input('Try doing the same thing again, press enter: '))
+    if cin2222 == "a":
+        cin2222 = "4"
+    elif cin2222 == "b":
+        cin2222 = "$"
+    elif cin2222 == "c":
+        cin2222 = "="
+    elif cin2222 == "d":
+        cin2222 = "-"
+    elif cin2222 == "e":
+        cin2222 = "X"
+    elif cin2222 == "f":
+        cin2222 = "#"
+    elif cin2222 == "g":
+        cin2222 = "$"
+    elif cin2222 == "o":
+        cin2222 = "]]"
+    elif cin2222 == "h":
+        cin2222 = ";"
+    elif cin2222 == "i":
+        cin2222 = "+"
+    elif cin2222 == "j":
+        cin2222 = "P"
+    elif cin2222 == "k":
+        cin2222 = "IK"
+    elif cin2222 == "l":
+        cin2222 = "\p"
+    elif cin2222 == "m":
+        cin2222 = "\\"
+    elif cin2222 == "n":
+        cin2222 = "7"
+    elif cin2222 == "p":
+        cin2222 = "69"
+    elif cin2222 == "q":
+        cin2222 = "oU"
+    elif cin2222 == "r":
+        cin2222 = "1q"
+    elif cin2222 == "s":
+        cin2222 = "I="
+    elif cin2222 == "t":
+        cin2222 = "po"
+    elif cin2222 == "u":
+        cin2222 = "90"
+    elif cin2222 == "v":
+        cin2222 = "Xc"
+    elif cin2222 == "w":
+        cin2222 = "o#"
+    elif cin2222 == "x":
+        cin2222 = "pl"
+    elif cin2222 == "x":
+        cin2222 = "opl"
+    elif cin2222 == "x":
+        cin2222 = "@@"
+    elif cin2222 == "y":
+        cin2222 = "%4"
+    elif cin2222 == "z":
+        cin2222 = "^&"
+    #Exagerration
+    print("process complete")
+    resultV = cin + cin2 + cin22 + cin222 + cin2222
+    print("here's your code:", resultV)
+
+passwerd = str(input("enter the password please (in capital without any brackets) "))
+if passwerd == "AYUSH":
+    print("ENTRY ACCESSED")
+    print("welcome to Encrypter Hexapent \n Type an individual lowercase letter one by one until system tell you to stop \n it will then encrypt your code and voila! you will have your very own encrypted password \n This version can only encrypt a 5 or 6 letters one by one \n \n don't worry, Your 6 symbol code can possibly be converted into a 7 or even 8 digit code")
+    print("please choose your code strength (Type a 5 or 6): ")
+    code_strength = int(input("Type here:"))
+    if code_strength == 5:
+        encryptV()
+    elif code_strength == 6:
+        encryptVI()
+else:
+    print("ENTRY DENIED")
+    print("Requesting program restart")
---
+++ Encrypter version_5.0.py
@@ -0,0 +1,299 @@
+def encryptV():
+    cin = str(input('enter individual alphabet of code and press enter'))
+    if cin == "a":
+        cin = "4"
+    elif cin == "b":
+        cin = "$"
+    elif cin == "c":
+        cin = "="
+    elif cin == "d":
+        cin = "-"
+    elif cin == "e":
+        cin = "X"
+    elif cin == "f":
+        cin = "#"
+    elif cin == "g":
+        cin = "$"
+    elif cin == "o":
+        cin = "]]"
+    elif cin == "h":
+        cin = ";"
+    elif cin == "i":
+        cin = "+"
+    elif cin == "j":
+        cin = "P"
+    elif cin == "k":
+        cin = "IK"
+    elif cin == "l":
+        cin = '\+'
+    elif cin == "m":
+        cin = "\\"
+    elif cin == "n":
+        cin = "7"
+    elif cin == "p":
+        cin = "69"
+    elif cin == "q":
+        cin = "oU"
+    elif cin == "r":
+        cin = "1q"
+    elif cin == "s":
+        cin = "I="
+    elif cin == "t":
+        cin = "po"
+    elif cin == "u":
+        cin = "90"
+    elif cin == "v":
+        cin = "Xc"
+    elif cin == "w":
+        cin = "o#"
+    elif cin == "x":
+        cin = "pl"
+    elif cin == "x":
+        cin = "opl"
+    elif cin == "x":
+        cin = "@@"
+    elif cin == "y":
+        cin = "%4"
+    elif cin == "z":
+        cin = "^&"
+    cin2 = str(input('enter individual alphabet of code and press enter'))
+    if cin2 == "a":
+        cin2 = "4"
+    elif cin2 == "b":
+        cin2 = "$"
+    elif cin2 == "c":
+        cin2 = "="
+    elif cin2 == "d":
+        cin2 = "-"
+    elif cin2 == "e":
+        cin2 = "X"
+    elif cin2 == "f":
+        cin2 = "#"
+    elif cin2 == "g":
+        cin2 = "$"
+    elif cin2 == "o":
+        cin2 = "]]"
+    elif cin2 == "h":
+        cin2 = ";"
+    elif cin2 == "i":
+        cin2 = "+"
+    elif cin2 == "j":
+        cin2 = "P"
+    elif cin2 == "k":
+        cin2 = "IK"
+    elif cin2 == "l":
+        cin2 = "\-0"
+    elif cin2 == "m":
+        cin2 = "\\"
+    elif cin2 == "n":
+        cin2 = "7"
+    elif cin2 == "p":
+        cin2 = "69"
+    elif cin2 == "q":
+        cin2 = "oU"
+    elif cin2 == "r":
+        cin2 = "1q"
+    elif cin2 == "s":
+        cin2 = "I="
+    elif cin2 == "t":
+        cin2 = "po"
+    elif cin2 == "u":
+        cin2 = "90"
+    elif cin2 == "v":
+        cin2 = "Xc"
+    elif cin2 == "w":
+        cin2 = "o#"
+    elif cin2 == "x":
+        cin2 = "pl"
+    elif cin2 == "x":
+        cin2 = "opl"
+    elif cin2 == "x":
+        cin2 = "@@"
+    elif cin2 == "y":
+        cin2 = "%4"
+    elif cin2 == "z":
+        cin2 = "^&"
+    cin22 = str(input('enter individual alphabet of code and press enter'))
+    if cin22 == "a":
+        cin22 = "4"
+    elif cin22 == "b":
+        cin22 = "$"
+    elif cin22 == "c":
+        cin22 = "="
+    elif cin22 == "d":
+        cin22 = "-"
+    elif cin22 == "e":
+        cin22 = "X"
+    elif cin22 == "f":
+        cin22 = "#"
+    elif cin22 == "g":
+        cin22 = "$"
+    elif cin22 == "o":
+        cin22 = "]]"
+    elif cin22 == "h":
+        cin22 = ";"
+    elif cin22 == "i":
+        cin22 = "+"
+    elif cin22 == "j":
+        cin22 = "P"
+    elif cin22 == "k":
+        cin22 = "IK"
+    elif cin22 == "l":
+        cin22 = "\}}"
+    elif cin22 == "m":
+        cin22 = "\\"
+    elif cin22 == "n":
+        cin22 = "7"
+    elif cin22 == "p":
+        cin22 = "69"
+    elif cin22 == "q":
+        cin22 = "oU"
+    elif cin22 == "r":
+        cin22 = "1q"
+    elif cin22 == "s":
+        cin22 = "I="
+    elif cin22 == "t":
+        cin22 = "po"
+    elif cin22 == "u":
+        cin22 = "90"
+    elif cin22 == "v":
+        cin22 = "Xc"
+    elif cin22 == "w":
+        cin22 = "o#"
+    elif cin22 == "x":
+        cin22 = "pl"
+    elif cin22 == "x":
+        cin22 = "opl"
+    elif cin22 == "x":
+        cin22 = "@@"
+    elif cin22 == "y":
+        cin22 = "%4"
+    elif cin22 == "z":
+        cin22 = "^&"
+    cin222 = str(input('enter individual alphabet of code and press enter'))
+    if cin222 == "a":
+        cin222 = "4"
+    elif cin222 == "b":
+        cin222 = "$"
+    elif cin222 == "c":
+        cin222 = "="
+    elif cin222 == "d":
+        cin222 = "-"
+    elif cin222 == "e":
+        cin222 = "X"
+    elif cin222 == "f":
+        cin222 = "#"
+    elif cin222 == "g":
+        cin222 = "$"
+    elif cin222 == "o":
+        cin222 = "]]"
+    elif cin222 == "h":
+        cin222 = ";"
+    elif cin222 == "i":
+        cin222 = "+"
+    elif cin222 == "j":
+        cin222 = "P"
+    elif cin222 == "k":
+        cin222 = "IK"
+    elif cin222 == "l":
+        cin222 = "\:"
+    elif cin222 == "m":
+        cin222 = "\\"
+    elif cin222 == "n":
+        cin222 = "7"
+    elif cin222 == "p":
+        cin222 = "69"
+    elif cin222 == "q":
+        cin222 = "oU"
+    elif cin222 == "r":
+        cin222 = "1q"
+    elif cin222 == "s":
+        cin222 = "I="
+    elif cin222 == "t":
+        cin222 = "po"
+    elif cin222 == "u":
+        cin222 = "90"
+    elif cin222 == "v":
+        cin222 = "Xc"
+    elif cin222 == "w":
+        cin222 = "o#"
+    elif cin222 == "x":
+        cin222 = "pl"
+    elif cin222 == "x":
+        cin222 = "opl"
+    elif cin222 == "x":
+        cin222 = "@@"
+    elif cin222 == "y":
+        cin222 = "%4"
+    elif cin222 == "z":
+        cin222 = "^&"
+    cin2222 = str(input('enter individual alphabet of code and press enter'))
+    if cin2222 == "a":
+        cin2222 = "4"
+    elif cin2222 == "b":
+        cin2222 = "$"
+    elif cin2222 == "c":
+        cin2222 = "="
+    elif cin2222 == "d":
+        cin2222 = "-"
+    elif cin2222 == "e":
+        cin2222 = "X"
+    elif cin2222 == "f":
+        cin2222 = "#"
+    elif cin2222 == "g":
+        cin2222 = "$"
+    elif cin2222 == "o":
+        cin2222 = "]]"
+    elif cin2222 == "h":
+        cin2222 = ";"
+    elif cin2222 == "i":
+        cin2222 = "+"
+    elif cin2222 == "j":
+        cin2222 = "P"
+    elif cin2222 == "k":
+        cin2222 = "IK"
+    elif cin2222 == "l":
+        cin2222 = "\p"
+    elif cin2222 == "m":
+        cin2222 = "\\"
+    elif cin2222 == "n":
+        cin2222 = "7"
+    elif cin2222 == "p":
+        cin2222 = "69"
+    elif cin2222 == "q":
+        cin2222 = "oU"
+    elif cin2222 == "r":
+        cin2222 = "1q"
+    elif cin2222 == "s":
+        cin2222 = "I="
+    elif cin2222 == "t":
+        cin2222 = "po"
+    elif cin2222 == "u":
+        cin2222 = "90"
+    elif cin2222 == "v":
+        cin2222 = "Xc"
+    elif cin2222 == "w":
+        cin2222 = "o#"
+    elif cin2222 == "x":
+        cin2222 = "pl"
+    elif cin2222 == "x":
+        cin2222 = "opl"
+    elif cin2222 == "x":
+        cin2222 = "@@"
+    elif cin2222 == "y":
+        cin2222 = "%4"
+    elif cin2222 == "z":
+        cin2222 = "^&"
+    print("process complete")
+    resultV = cin + cin2 + cin22 + cin222 + cin2222
+    print(resultV)
+    print("here's your code!")
+#password
+password = str(input("enter the password please (in capital without any brackets) "))
+if password == "AYUSH":
+    print("ENTRY ACCESSED")
+    print("welcome to encrypter X66 \n Type an individual owercase letter one by one until system tell you to stop \n it will then encrypt your code and voila! you have your very own encrypted code \n This can only store a 5 letter code")
+    encryptV()
+else:
+    print("ENTRY DENIED")
+    print("Requesting program restart")
